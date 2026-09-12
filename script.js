fa42502a5a2b8747694689d2961367a7eab2aabb
KMukendi10 <kazadishadrackmukendi@gmail.com>
2026-09-12 13:37:01 +0200

    Projects card update

--- script.js
+++ script.js
@@ -317,7 +317,8 @@ function initScrollFade() {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('visible');
-                observer.unobserve(entry.target);
+            } else {
+                entry.target.classList.remove('visible');
             }
         });
     }, { threshold: 0.15 });
